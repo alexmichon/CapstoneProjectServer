afff882ed7d0e0ed9235bd24485e68636d007656
Alex Michon <alexmichon@hotmail.fr>
2017-10-19 11:46:28 -0700

    Setup devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-	namespace :api do
-		namespace :v1 do
-		end
-	end
+  namespace :api do
+    namespace :v1 do
+    end
+  end
+  devise_for :doctors
+  devise_for :patients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
